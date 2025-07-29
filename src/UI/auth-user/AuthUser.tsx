7c1999a103fc1d33ee36da4ce4bf5332224d98f1
видеокал- <dich140804@gmail.com>
2025-07-29 21:48:31 +0300

    Working for UI.

---
+++ src/UI/auth-user/AuthUser.tsx
@@ -0,0 +1,12 @@
+import styles from './auth-user.module.css'
+
+function AuthUser() {
+  return (
+      <div className={styles.container}>
+          видеокал-
+          <div className={styles.avatar}></div>
+      </div>
+  )
+}
+
+export default AuthUser;
---
+++ src/UI/main-logo/MainLogo.tsx
@@ -0,0 +1,11 @@
+import styles from './main-logo.module.css'
+
+function MainLogo() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.text}>MONOPOLY</div>
+    </div>
+  )
+}
+
+export default MainLogo;
