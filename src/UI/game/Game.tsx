829f8172a1dcaad9898679c9174b8266400d4919
видеокал- <dich140804@gmail.com>
2025-08-01 12:03:39 +0300

    Working for UI.

--- src/UI/game/Game.tsx
+++ src/UI/game/Game.tsx
@@ -1,3 +1,4 @@
+import Angle, { AngleTypes } from '../game-fiields/Angle/Angle';
 import Property from '../game-fiields/Property/Property';
 import Special, { SpecialTypes } from '../game-fiields/Special/Special';
 import styles from './game.module.css'
@@ -5,7 +6,9 @@ import styles from './game.module.css'
 function game() {
   return (
     <div className={styles.container}>
-      <div className={`${styles.section} ${styles.stop}`}>Бесплатная стоянка</div>
+      <div className={`${styles.section} ${styles.stop}`}>
+        <Angle type={AngleTypes.FREE_PARKING} />
+      </div>
       <div className={`${styles.section} ${styles.top_section}`}>
         <Property orientation='top' color='#c02525ff' name='УЛ. ТВЕРСКАЯ' />
         <Special orientation='top' type={SpecialTypes.CHANCE} name='ШАНС' />
@@ -17,43 +20,49 @@ function game() {
         <Special orientation='top' type={SpecialTypes.UTILITY} name='ВОДОПРОВОД' />
         <Property orientation='top' color='#f0ec14ff' name='СМОЛЕНСКАЯ ПЛОЩАДЬ' />
       </div>
-      <div className={`${styles.section} ${styles.goto_prison}`}>В тюрьму</div>
+      <div className={`${styles.section} ${styles.goto_prison}`}>
+        <Angle type={AngleTypes.JAIL} />
+      </div>
       <div className={`${styles.section} ${styles.left_section}`}>
-        <Property orientation='left' color='#f38823ff' name='РУБЛЕВСКОЕ ШОССЕ'/>
-        <Property orientation='left' color='#f38823ff' name='УЛ. ВАВИЛОВА'/>
-        <Special orientation='left' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА'/>
-        <Property orientation='left' color='#f38823ff' name='РЯЗАНСКИЙ ПРОСПЕКТ'/>
-        <Special orientation='left' type={SpecialTypes.RAILROAD} name='КУРСКАЯ ЖЕЛЕЗНАЯ ДОРОГА'/>
-        <Property orientation='left' color='#cf2a5bff' name='РОСТОВСКАЯ НАБ.'/>
-        <Property orientation='left' color='#cf2a5bff' name='УЛ. СРЕТЕНКА'/>
-        <Special orientation='left' type={SpecialTypes.UTILITY} name='ЭЛЕКТРИЧЕСТВО'/>
-        <Property orientation='left' color='#cf2a5bff' name='УЛ. ПОЛЯНКА'/>
+        <Property orientation='left' color='#f38823ff' name='РУБЛЕВСКОЕ ШОССЕ' />
+        <Property orientation='left' color='#f38823ff' name='УЛ. ВАВИЛОВА' />
+        <Special orientation='left' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА' />
+        <Property orientation='left' color='#f38823ff' name='РЯЗАНСКИЙ ПРОСПЕКТ' />
+        <Special orientation='left' type={SpecialTypes.RAILROAD} name='КУРСКАЯ ЖЕЛЕЗНАЯ ДОРОГА' />
+        <Property orientation='left' color='#cf2a5bff' name='РОСТОВСКАЯ НАБ.' />
+        <Property orientation='left' color='#cf2a5bff' name='УЛ. СРЕТЕНКА' />
+        <Special orientation='left' type={SpecialTypes.UTILITY} name='ЭЛЕКТРИЧЕСТВО' />
+        <Property orientation='left' color='#cf2a5bff' name='УЛ. ПОЛЯНКА' />
       </div>
       <div className={`${styles.section} ${styles.chat_section}`}>Секция чата</div>
       <div className={`${styles.section} ${styles.right_section}`}>
-        <Property orientation='right' color='#1b7928ff' name='УЛ. ЩУСЕВА'/>
-        <Property orientation='right' color='#1b7928ff' name='ГОГОЛЕВКИЙ БУЛЬВАР'/>
-        <Special orientation='right' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА'/>
-        <Property orientation='right' color='#1b7928ff' name='КУТУЗОВСКИЙ ПРОСПЕКТ'/>
-        <Special orientation='right' type={SpecialTypes.RAILROAD} name='ЛЕНИНГРАДСКАЯ ЖЕЛЕЗНАЯ ДОРОГА'/>
-        <Special orientation='right' type={SpecialTypes.CHANCE} name='ШАНС'/>
-        <Property orientation='right' color='#274abeff' name='УЛ. МАЛАЯ БРОННАЯ'/>
-        <Special orientation='right' type={SpecialTypes.TAX} name='СВЕРХНАЛОГ'/>
-        <Property orientation='right' color='#274abeff' name='УЛ. АРБАТ'/>
+        <Property orientation='right' color='#1b7928ff' name='УЛ. ЩУСЕВА' />
+        <Property orientation='right' color='#1b7928ff' name='ГОГОЛЕВКИЙ БУЛЬВАР' />
+        <Special orientation='right' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА' />
+        <Property orientation='right' color='#1b7928ff' name='КУТУЗОВСКИЙ ПРОСПЕКТ' />
+        <Special orientation='right' type={SpecialTypes.RAILROAD} name='ЛЕНИНГРАДСКАЯ ЖЕЛЕЗНАЯ ДОРОГА' />
+        <Special orientation='right' type={SpecialTypes.CHANCE} name='ШАНС' />
+        <Property orientation='right' color='#274abeff' name='УЛ. МАЛАЯ БРОННАЯ' />
+        <Special orientation='right' type={SpecialTypes.TAX} name='СВЕРХНАЛОГ' />
+        <Property orientation='right' color='#274abeff' name='УЛ. АРБАТ' />
       </div>
-      <div className={`${styles.section} ${styles.forward_joly}`}>Посетить тюрьму</div>
+      <div className={`${styles.section} ${styles.forward_joly}`}>
+        <Angle type={AngleTypes.JUST_VISITING} />
+      </div>
       <div className={`${styles.section} ${styles.bottom_section}`}>
-        <Property orientation='bottom' color='#b9d7ffff' name='ПЕРВАЯ ПАРКОВАЯ УЛ.'/>
-        <Property orientation='bottom' color='#b9d7ffff' name='УЛ. ОГЕЕВА'/>
-        <Special orientation='bottom' type={SpecialTypes.CHANCE} name='ШАНС'/>
-        <Property orientation='bottom' color='#b9d7ffff' name='ВАРШАВСКОЕ ШОССЕ'/>
-        <Special orientation='bottom' type={SpecialTypes.RAILROAD} name='РИЖСКАЯ ЖЕЛЕЗНАЯ ДОРОГА'/>
-        <Special orientation='bottom' type={SpecialTypes.TAX} name='ПОДОХОДНЫЙ НАЛОГ'/>
-        <Property orientation='bottom' color='#831717ff' name='НАГАТИНСКАЯ УЛ.'/>
-        <Special orientation='bottom' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА'/>
-        <Property orientation='bottom' color='#831717ff' name='ЖИТНАЯ УЛ.'/>
+        <Property orientation='bottom' color='#b9d7ffff' name='ПЕРВАЯ ПАРКОВАЯ УЛ.' />
+        <Property orientation='bottom' color='#b9d7ffff' name='УЛ. ОГЕЕВА' />
+        <Special orientation='bottom' type={SpecialTypes.CHANCE} name='ШАНС' />
+        <Property orientation='bottom' color='#b9d7ffff' name='ВАРШАВСКОЕ ШОССЕ' />
+        <Special orientation='bottom' type={SpecialTypes.RAILROAD} name='РИЖСКАЯ ЖЕЛЕЗНАЯ ДОРОГА' />
+        <Special orientation='bottom' type={SpecialTypes.TAX} name='ПОДОХОДНЫЙ НАЛОГ' />
+        <Property orientation='bottom' color='#831717ff' name='НАГАТИНСКАЯ УЛ.' />
+        <Special orientation='bottom' type={SpecialTypes.COMMUNITY_CHEST} name='ОБЩЕСТВЕННАЯ КАЗНА' />
+        <Property orientation='bottom' color='#831717ff' name='ЖИТНАЯ УЛ.' />
       </div>
-      <div className={`${styles.section} ${styles.start}`}>Вперёд</div>
+      <div className={`${styles.section} ${styles.start}`}>
+        <Angle type={AngleTypes.GO} />
+      </div>
     </div>
   )
 }
